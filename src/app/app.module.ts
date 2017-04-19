ca5469a751806065c77ff67f23bd2cbf155ae6e6
PLDiCicco <peter@nsmithfieldri.org>
2017-04-19 11:01:28 -0400

    add framework module and main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FrameworkModule } from '../framework/framework.module'
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    FrameworkModule
   ],
   providers: [],
   bootstrap: [AppComponent]
